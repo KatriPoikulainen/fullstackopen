a931183053a15f43ee85098e5da7929625425444
Katri Poikulainen <katri.poikulainen@student.hamk.fi>
2026-02-06 17:37:21 +0200

    unicafestep2

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [hyva, setHyva] = useState(0)
   const [neutraali, setNeutraali] = useState(0)
   const [huono, setHuono] = useState(0)
+  const yhteensa = hyva+neutraali+huono
+  const keskiarvo = yhteensa === 0?0: (hyva-huono)/yhteensa
+  const tyytyvaisyys = yhteensa === 0?0: (hyva/yhteensa)*100
 
   return (
     <div>
@@ -15,8 +17,11 @@ const App = () => {
 
       <h2>tilastot</h2>
       <p>hyva {hyva}</p>
-      <p>neutraali{neutraali}</p>
-      <p>huono{huono}</p>
+      <p>neutraali {neutraali}</p>
+      <p>huono {huono}</p>
+      <p>yhteensa {yhteensa}</p>
+      <p>keskiarvo {keskiarvo}</p>
+      <p>tyytyvaisyys {tyytyvaisyys} %</p>
 
 
     </div>
