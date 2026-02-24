f5857cc3db13fa125df272483c37bf3cea0c0b29
Katri Poikulainen <katri.poikulainen@student.hamk.fi>
2026-02-24 19:19:00 +0200

    kurssitiedot course-moduuli

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,43 +1,4 @@
-const Header = (props) => {
-  return<h1>{props.course.name}</h1>
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.parts.map((part)=>(
-        <Part key={part.id} part={part}/>
-      ))}
-</div>
-  )
-}
-
-const Total = (props) => {
-const total = props.parts.reduce ((sum, part) => {
-  console.log ("Number of exercises", sum, part)
-  return sum + part.exercises
-}, 0)
-
-return <p>Number of exercises {total}</p>
-}
-
-const Course = (props) => {
-  return(
-    <div>
-      <Header course={props.course}/>
-      <Content parts={props.course.parts}/>
-      <Total parts= {props.course.parts}/>
-    </div>
-  )
-}
+import Course from "./Course"
 
 const App = () => {
   const courses = [
